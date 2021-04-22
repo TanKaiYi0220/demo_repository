b603845338e9ac247e9166e3cad517010d0a3d90
TanKaiYi0220 <60723049+TanKaiYi0220@users.noreply.github.com>
2021-04-22 21:18:09 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,4 @@
+print("\"git init\" used to create a git repository")
+print("There are local area & staging area")
+print("Local area is the area that located at the git init location")
+print("\"git status\" used to check status of this git repository")
